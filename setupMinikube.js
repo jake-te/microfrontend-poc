17b4fd84a9e6c95206efbe94e52e70c4ea5fd0ca
Adam Wilson <adam@thousandeyes.com>
2020-04-09 16:53:51 -0400

    STash

--- setupMinikube.js
+++ setupMinikube.js
@@ -1,35 +1,22 @@
 // Note: Requires
+// - minikube installed
 // - minikube with ingress addon installed
-// - kubectl with valid context 'minikube'
 
 const childProcess = require('child_process');
 const fs = require('fs');
 const os = require('os');
 
-// minikube delete
-// minikube start
-
-// TODO: Prob doesn't work
-const command = isWindows() ? '& minikube -p minikube docker-env | Invoke-Expression'
-                            : 'eval $(minikube docker-env)';
-
-childProcess.execSync(command);
-
-// TODO:
-// 1) Build working docker image for each locally, make sure it serves what we want
-// 2) Add commands here for building those in minikube
-
-// build each image in docker
-// docker build -t nodejs-server .
-
-
 // Clear out minikube environment
-kubectl('delete deployments --all');
-kubectl('delete ingress --all');
-kubectl('delete svc --all');
+minikubeKubectl('delete deployments --all');
+minikubeKubectl('delete ingress --all');
+minikubeKubectl('delete svc --all');
+
+// Build each app image in minikube's docker context
+runWithMinikubeDocker('npm --prefix ./webapps run build.image')
+runWithMinikubeDocker('npm --prefix ./endpoint-agent run build.image')
+
 
 // TODO: Generalize this
-// Create k8s objects
 applyK8sFiles('./endpoint-agent/k8s')
 applyK8sFiles('./webapps/k8s')
 
@@ -37,14 +24,30 @@ applyK8sFiles('./webapps/k8s')
 function applyK8sFiles(directory) {
     const files = fs.readdirSync(directory);
     for (const file of files) {
-        kubectl(`apply -f ${directory}/${file}`);
+        minikubeKubectl(`apply -f ${directory}/${file}`);
     }
 
 }
-function kubectl(command) {
-    childProcess.execSync(`kubectl --context minikube ${command}`);
+
+// Uses minikube's kubectl
+function minikubeKubectl(command) {
+    return executeCommand(`minikube kubectl -- ${command}`);
+}
+
+function runWithMinikubeDocker(command) {
+    // `@FOR /f "tokens=*" %i IN ('minikube -p minikube docker-env') DO @%i`
+    const setDockerToMinikubeContext = isWindows() ? `& minikube -p minikube docker-env | Invoke-Expression`
+                                                   : 'eval $(minikube docker-env)';
+
+    return executeCommand(`${setDockerToMinikubeContext}; ${command}`);
 }
 
 function isWindows() {
     return os.type() === 'Windows_NT';
+}
+
+
+function executeCommand(command) {
+    const options = isWindows() ? { shell: 'powershell' } : undefined;
+    return childProcess.execSync(command, options)
 }
