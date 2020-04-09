67e0b407d03cdf9bfc3dfb8aedca0c846b78df12
Adam Wilson <adam@thousandeyes.com>
2020-04-09 15:48:05 -0400

    STASH

--- webapps/buildStaticAssets.js
+++
@@ -1,4 +0,0 @@
-const childProcess = require('child_process');
-
-const staticAssetsBuildDir = `dist/public`;
-childProcess.execSync(`npx vue-cli-service build --filename webapps --dest ${staticAssetsBuildDir} --mode development --inline-vue --no-clean ./src/client/main.js && npx rimraf ${staticAssetsBuildDir}/demo.html`);
